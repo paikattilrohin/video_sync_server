20bac58c3a00a7c4d61a0c8feb584d47adea9c43
Rohin Paikattil <paikattilrohin@gmail.com>
2022-02-12 12:53:54 -0500

    Code clean up and New server migration

--- sync_server.js
+++ sync_server.js
@@ -28,7 +28,6 @@ const rateLimiter = new RateLimiterMemory(rateLimiterOptions);
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, (msg) => {
-  // console.log(`Listening on ${PORT}`);
   console.log(msg);
 });
 
@@ -39,9 +38,8 @@ let wake = false;
 function wakeServer(status) {
   if (status) {
     wake = setInterval( () => {
-      // console.log("running Every 1 minute");
       if (!debug) {
-        http.get('http://syncevent.herokuapp.com');
+        http.get('https://video-sync-extension-server.herokuapp.com/');
       }
     }, wakeServerTime * 60000);
 
@@ -57,9 +55,6 @@ wakeServer(true);
 
 
 io.on("connection", (socket) => {
-  console.log("------------------------------------------------------------------------------------");
-  console.log(" connecting socket ", socket.id);
-
 
   socket.on('enter', (login_details) => {
 
@@ -84,9 +79,6 @@ io.on("connection", (socket) => {
       socket.emit('enter', { successful: false, reason: "Wrong Secret Key" });
       socket.disconnect();
     }
-    console.log("all rooms", all_rooms);
-    console.log("socket to rooms", socket_connections_to_room);
-
   });
 
 
@@ -98,17 +90,13 @@ io.on("connection", (socket) => {
       socket.leave(roomname);
       delete all_rooms[roomname]["connected_users"][socket.id];
       all_rooms[roomname].count--;
-      //////////////// CHANGE THIS TO < 1 ----------------------------------------------------------------------------------------------- Important
       if (all_rooms[roomname].count == 5) {
         delete all_rooms[roomname];
       }
       else {
-        console.log("update user list ");
         socket.broadcast.to(roomname).emit('connected_users', all_rooms[roomname]);
       }
       delete socket_connections_to_room[socket.id];
-      console.log("all rooms", all_rooms);
-      console.log("socket to rooms", socket_connections_to_room);
     }
   });
 
@@ -126,19 +114,17 @@ io.on("connection", (socket) => {
     console.log("-> time requested");
     let roomname = socket_connections_to_room[socket.id];
     all_rooms[roomname].time_requested = true;
-    socket.broadcast.to(roomname).emit("get_time", {});   // <------change this while locally testing
-    // socket.emit("get_time", {});   // <-----   change to this later while locally testing
+    socket.broadcast.to(roomname).emit("get_time", {});   
+    
   });
 
 
 
   socket.on('get_time', (data)=>{
-    // time sent back to server
-    console.log("-> time given back is \n ", data);
     let roomname = socket_connections_to_room[socket.id];
     if(all_rooms[roomname].time_requested == true){
-      socket.broadcast.to(roomname).emit("start_video", data);  // <-----   change to this later while locally testing
-      // socket.emit("transmit_video_event", data); // <-----   change to this later while locally testing
+      socket.broadcast.to(roomname).emit("start_video", data); 
+      
       all_rooms[roomname].time_requested = false;
     }
   });
@@ -148,86 +134,8 @@ io.on("connection", (socket) => {
     let roomname = socket_connections_to_room[socket.id];
 
   
-    socket.broadcast.to(roomname).emit("transmit_video_event", data);   // <----- change this while testing locally  
-    // socket.emit("transmit_video_event",data);  // <-----   change to this later while locally testing
-  });
-
-
-
-
-  socket.onAny((event, data) => {
-    console.log("GENERAL -------event is this:", event, " \n data is \n", data);
+    socket.broadcast.to(roomname).emit("transmit_video_event", data);   
   });
 
 
 });
-
-
-  // socket.on('disconnect', () => {
-  //   console.log("deleting socket", socket.id);
-  //   if (socket_connections_to_room[socket.id]) {
-  //     let roomname = socket_connections_to_room[socket.id];
-  //     socket.leave(socket_connections_to_room[socket.id]);
-  //     delete all_rooms[socket_connections_to_room[socket.id]][socket.id];
-  //     if (all_rooms[socket_connections_to_room[socket.id]] &&
-  //       Object.keys(all_rooms[socket_connections_to_room[socket.id]]).length === 0) {
-  //       console.log("deleting room ", socket_connections_to_room[socket.id]);
-  //       delete all_rooms[socket_connections_to_room[socket.id]];
-  //     }
-  //     if (all_rooms[socket_connections_to_room[socket.id]]) {
-  //       console.log("update user list ");
-  //       socket.broadcast.to(roomname).emit('connectedUsers', all_rooms[socket_connections_to_room[socket.id]]);
-  //     }
-  //     console.log("all rooms",all_rooms);
-  //     console.log("socket to rooms",socket_connections_to_room);
-  //   }
-  // });
-
-
-
-
-
-  // socket_connections_to_room[socket.id] = null;
-
-
-  // socket.on('enter', (login_details) => {
-  //   console.log(login_details);
-
-  //   if (login_details.secretkey === secretkey ) {
-
-  //     if(socket_connections_to_room[socket.id]){
-  //       // already connected to a room 
-  //       let roomname = socket_connections_to_room[socket.id];
-  //       delete all_rooms[socket_connections_to_room[socket.id]][socket.id];
-  //       if (all_rooms[socket_connections_to_room[socket.id]] &&
-  //         Object.keys(all_rooms[socket_connections_to_room[socket.id]]).length === 0) {
-  //         console.log("deleting room ", socket_connections_to_room[socket.id]);
-  //         delete all_rooms[socket_connections_to_room[socket.id]];
-  //       }
-  //       if (all_rooms[socket_connections_to_room[socket.id]]) {
-  //         console.log("update user list ");
-  //         socket.broadcast.to(roomname).emit('connectedUsers', all_rooms[socket_connections_to_room[socket.id]]);
-  //       }
-  //       delete socket_connections_to_room[socket.id];
-  //     }
-  //     if (!(login_details.roomname in all_rooms)) {
-  //       all_rooms[login_details.roomname] = {};
-  //       all_rooms[login_details.roomname][socket.id] = null       
-  //     }
-
-  //     socket_connections_to_room[socket.id] = login_details.roomname;
-  //     all_rooms[login_details.roomname][socket.id] = login_details.username;
-  //     socket.join(login_details.roomname);
-  //     console.log("connected successfully");
-  //     socket.emit('enter', {successful : true});
-  //     console.log("all rooms",all_rooms);
-  //     console.log("socket to rooms",socket_connections_to_room);
-  //   }
-  //   else {
-  //     socket.send('enter', { successful: false });
-  //     delete socket_connections_to_room[socket.id];
-  //     socket.disconnect();
-  //   }
-  // });
-
-
